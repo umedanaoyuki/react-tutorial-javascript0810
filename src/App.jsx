9996c9618733a63be7a3cfe9495fabb444f25de0
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-11 22:11:20 +0900

    勝者を返す関数の作成

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,29 @@ export default function Board() {
   );
 }
 
+function calculateWinner(squares) {
+  // 勝ちになるマス目の２次元配列
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // lines配列の中身をa,b,cに代入
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      // 起点になるマス目に代入されている X or 0 を返却する
+      return squares[a];
+    }
+  }
+}
+
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
